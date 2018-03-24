ec5e389fc1973678b122c7972c44a74b08852709
JulianChu <yulang.chu@gmail.com>
2018-03-24 19:11:30 +0100

    init setting

---
+++ test/romanToInt.test.ts
@@ -0,0 +1,9 @@
+describe("", () => {
+  it("Should return 200 ok", () => {
+    let i: number = 1 + 1;
+    let add = function (x: number, y: number): number {
+      return x + y;
+    }
+    expect(add(1, 2)).toBe(3);
+  })}
+)

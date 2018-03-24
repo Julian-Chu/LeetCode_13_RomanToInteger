8719aa690ed3200cf157d0866d69f05edd6c56cc
JulianChu <yulang.chu@gmail.com>
2018-03-24 21:14:24 +0100

    --amend

--- test/romanToInt.test.ts
+++ test/romanToInt.test.ts
@@ -1,12 +1,9 @@
 let romanToInt = function (input: string): number {
   let value: number = 0;
   let roman: string | any = input.toUpperCase();
-  
   value = parseInt(romanNumerals[roman]);
-
   return value;
 }
-
 enum romanNumerals {
   I = 1,
   V = 5,
@@ -15,30 +12,24 @@ enum romanNumerals {
   C = 100,
   D = 500,
   M = 1000
-
 }
 
 describe("Single Alphabet", () => {
   it("give I, return 1", () => {
     expect(romanToInt("I")).toBe(1);
   });
-
   it("give V, return 5", () => {
     expect(romanToInt("V")).toBe(5);
   });
-
   it("give X, return 10", () => {
     expect(romanToInt("X")).toBe(10);
   });
-
   it("give L, return 50", () => {
     expect(romanToInt("L")).toBe(50);
   });
-
   it("give C, return 100", () => {
     expect(romanToInt("C")).toBe(100);
   });
-
   it("give D, return 500", () => {
     expect(romanToInt("D")).toBe(500);
   });

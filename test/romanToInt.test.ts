43d76da68b3baa4cb11e880866f90be2cd7706ee
JulianChu <yulang.chu@gmail.com>
2018-03-24 21:13:31 +0100

    input is single alphabet

--- test/romanToInt.test.ts
+++ test/romanToInt.test.ts
@@ -1,9 +1,48 @@
-describe("", () => {
-  it("Should return 200 ok", () => {
-    let i: number = 1 + 1;
-    let add = function (x: number, y: number): number {
-      return x + y;
-    }
-    expect(add(1, 2)).toBe(3);
-  })}
-)
+let romanToInt = function (input: string): number {
+  let value: number = 0;
+  let roman: string | any = input.toUpperCase();
+  
+  value = parseInt(romanNumerals[roman]);
+
+  return value;
+}
+
+enum romanNumerals {
+  I = 1,
+  V = 5,
+  X = 10,
+  L = 50,
+  C = 100,
+  D = 500,
+  M = 1000
+
+}
+
+describe("Single Alphabet", () => {
+  it("give I, return 1", () => {
+    expect(romanToInt("I")).toBe(1);
+  });
+
+  it("give V, return 5", () => {
+    expect(romanToInt("V")).toBe(5);
+  });
+
+  it("give X, return 10", () => {
+    expect(romanToInt("X")).toBe(10);
+  });
+
+  it("give L, return 50", () => {
+    expect(romanToInt("L")).toBe(50);
+  });
+
+  it("give C, return 100", () => {
+    expect(romanToInt("C")).toBe(100);
+  });
+
+  it("give D, return 500", () => {
+    expect(romanToInt("D")).toBe(500);
+  });
+  it("give M, return 1000", () => {
+    expect(romanToInt("M")).toBe(1000);
+  });
+})
